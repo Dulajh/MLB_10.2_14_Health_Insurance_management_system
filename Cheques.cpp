3b09e5f7b1a5e9c883b066deea7bcddacb4b4e68
it21387258 <101700336+it21387258@users.noreply.github.com>
2022-05-20 17:02:14 +0530

    Update Cheques.cpp
    //IT21387258
    //Bandara H.M.C.S
    
    //Cheques
    
    #include<iostream>
    #include <iomanip>
    #include <string>
    #define SIZE 2
    
    #include "Payment.h"
    #include "Cheques.h"
    
    Cheques::Cheques(string p_id, string p_date, float p_amount, string b_id, string
    branch)
    :Payment( p_id,p_date, p_amount)
    {
    bankID =b_id;
    Branch = branch;
    }
    void Cheques::setbankID(string b_id){
    bankID = b_id;
    }
    string Cheques::getbankID(){
    return bankID;
    }
    void Cheques::setbranch(string branch){
    }
    string getbranch(){
    }
    void Cheques::displayChequesDetails(){
    cout<<"Payment ID: "<<ID<<endl;
    cout<<"Payment Date: "<<Date<<endl;
    cout<<"Payment Amount: "<<Amount<<endl;
    cout<<"Bank ID: "<<bankID<<endl;
    cout<<"Branch: "<<Branch<<endl;
    }

--- Cheques.cpp
+++ Cheques.cpp
@@ -3,22 +3,35 @@
 
 //Cheques
 
+#include<iostream>
+#include <iomanip>
+#include <string>
+#define SIZE 2
+
 #include "Payment.h"
+#include "Cheques.h"
 
-class Cheques: public Payment{
-private:
-
-string bankID;
-string Branch;
-
-public:
-
-Cheques(string p_id, string p_date, float p_amount, string b_id, string
-branch);
-
-void setbankID(string b_id);
-string getbankID();
-void setbranch(string branch);
-string getbranch();
-void displayChequesDetails();
+Cheques::Cheques(string p_id, string p_date, float p_amount, string b_id, string
+branch)
+:Payment( p_id,p_date, p_amount)
+{
+bankID =b_id;
+Branch = branch;
+}
+void Cheques::setbankID(string b_id){
+bankID = b_id;
+}
+string Cheques::getbankID(){
+return bankID;
+}
+void Cheques::setbranch(string branch){
+}
+string getbranch(){
+}
+void Cheques::displayChequesDetails(){
+cout<<"Payment ID: "<<ID<<endl;
+cout<<"Payment Date: "<<Date<<endl;
+cout<<"Payment Amount: "<<Amount<<endl;
+cout<<"Bank ID: "<<bankID<<endl;
+cout<<"Branch: "<<Branch<<endl;
 }
